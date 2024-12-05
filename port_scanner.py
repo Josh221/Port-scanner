93379475e5eb4cd57721ab071cb644869b804ea0
Josh Bearne <90691439+Josh221@users.noreply.github.com>
2024-12-05 20:09:31 +0200

    Update port_scanner.py

--- port_scanner.py
+++ port_scanner.py
@@ -30,7 +30,7 @@ if __name__ == "__main__":
         target = input("Enter host for scanning: ")
         target_IP = gethostbyname(target)
         print(f"Start scanning on host: {target_IP}")
-        
+
         start_port = int(input("Enter the start port: "))
         end_port = int(input("Enter the end port: "))
         if start_port < 1 or end_port > 65535 or start_port > end_port:
@@ -38,14 +38,16 @@ if __name__ == "__main__":
             exit()
 
         start_time = time.time()
-        asyncio.run(scan_ports_concurrently(target_IP, start_port, end_port))
+        # Use the current event loop
+        loop = asyncio.get_event_loop()
+        loop.run_until_complete(scan_ports_concurrently(target_IP, start_port, end_port))
         print('Time taken: ', time.time() - start_time)
-        
+
         if open_ports:
             print(f"Open ports: {open_ports}")
         else:
             print("No open ports found.")
-    
+
     except gaierror:
         print("Error: Unable to resolve hostname. Please check the input.")
     except KeyboardInterrupt:
@@ -54,3 +56,5 @@ if __name__ == "__main__":
         print("Error: Invalid input. Please enter numeric values for ports.")
     except Exception as e:
         print(f"An unexpected error occurred: {e}")
+
+
